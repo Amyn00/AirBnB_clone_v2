311151574bf6b723fd85a843ddea1e4c81ef457d
amyn00 <medamine190@gmail.com>
2023-12-07 06:05:08 -0800

    update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -53,14 +53,15 @@ def do_deploy(archive_path):
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/released/{}/web_static/* /data/web_static/released/{}/".format(name, name)).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/released/{}/ /data/web_static/current".
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
