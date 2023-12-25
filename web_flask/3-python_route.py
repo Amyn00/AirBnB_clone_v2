4b520659b4d1d483195d3b9c196a54b4a11ca58f
Amyn00 <medamine190@gmail.com>
2023-12-25 00:57:42 -0800

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,6 +26,7 @@ def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 
+@app.route("/python")
 @app.route("/python/<text>")
 def python_text(text="is cool"):
     """ display Python text """
